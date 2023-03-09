53603adcc8de297899311913bfe01ea9492c9900
David Albert <developer@dtalbert.com>
2023-03-09 10:57:14 -0500

    removed counter functionality (from the flutter template) added 2 more fields to the form broke form fields into separate funcs add distinct TextEditingControllers for each field DEBUG prints out all 3 fields when you submit the form

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Mortgage Calculator',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Your Mortgage'),
@@ -49,30 +41,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final controller = TextEditingController();
+  final balanceController = TextEditingController();
+  final rateController = TextEditingController();
+  final termController = TextEditingController();
 
   @override
   void dispose() {
-    controller.dispose();
+    balanceController.dispose();
+    rateController.dispose();
+    termController.dispose();
     super.dispose();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _saveMortgage() {
+    setState(() {});
   }
 
+  // This method is rerun every time setState is called
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -87,57 +75,130 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Balance: "),
-                      ),
-                      Flexible(
-                        //This container is required when placing a TextFormField inside a Row <shrug> - https://stackoverflow.com/questions/45986093/textfield-inside-of-row-causes-layout-exception-unable-to-calculate-size
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            controller: controller,
-                            decoration: const InputDecoration(
-                                hintText:
-                                    'Enter the principle balance of your mortgage',
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.red,
-                                    width: 10,
-                                  ),
-                                ),
-                                filled: true,
-                                fillColor: Color.fromARGB(255, 169, 209, 229)),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter a number';
-                              }
-                              return null;
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  balanceField(),
+                  rateField(),
+                  termField(),
                   ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
                         //Process form data
-                        debugPrint('$_counter');
-                        debugPrint(controller.text);
+                        debugPrint("balance: ${balanceController.text}");
+                        debugPrint("rate: ${rateController.text}");
+                        debugPrint("term: ${termController.text}");
                       }
                     },
                     child: const Text('Submit'),
                   )
                 ],
               ))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  Widget balanceField() {
+    return Row(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("Balance: "),
+        ),
+        Flexible(
+          //This container is required when placing a TextFormField inside a Row <shrug> - https://stackoverflow.com/questions/45986093/textfield-inside-of-row-causes-layout-exception-unable-to-calculate-size
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: balanceController,
+              decoration: const InputDecoration(
+                  hintText: 'Enter the principle balance of your mortgage',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 10,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 169, 209, 229)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a number';
+                }
+                return null;
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget rateField() {
+    return Row(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("Interest Rate: "),
+        ),
+        Flexible(
+          //This container is required when placing a TextFormField inside a Row <shrug> - https://stackoverflow.com/questions/45986093/textfield-inside-of-row-causes-layout-exception-unable-to-calculate-size
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: rateController,
+              decoration: const InputDecoration(
+                  hintText: 'Enter the interest rate of your mortgage',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.yellow,
+                      width: 10,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 169, 209, 229)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a number btwn 0.0 - 100.0';
+                }
+                return null;
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget termField() {
+    return Row(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("Term (months): "),
+        ),
+        Flexible(
+          //This container is required when placing a TextFormField inside a Row <shrug> - https://stackoverflow.com/questions/45986093/textfield-inside-of-row-causes-layout-exception-unable-to-calculate-size
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: termController,
+              decoration: const InputDecoration(
+                  hintText: 'Enter the length of your mortgage (months)',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.yellow,
+                      width: 10,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 169, 209, 229)),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a number btwn 0.0 - 100.0';
+                }
+                return null;
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
