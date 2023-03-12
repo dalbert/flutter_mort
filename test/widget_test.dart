86e6a9ee3b0659ef82965019eecf9e2dcfa916e9
David Albert <developer@dtalbert.com>
2023-03-11 19:42:57 -0500

    Tried to re-add the widget_test, ie UI tests, but so far I have not gotten the very first thing to work Finding a widget with the text "Balance:" inside it fails. I don't know why, this appears to me to be precisely analogous to the original test that found the counter widget by searching for text "0" IDFK

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,29 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_mort/mortgage.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_mort/main.dart';
 
-void main() {}
+void main() {
+  testWidgets('Spot checking that a few UI widgets are rendered. ',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Balance:'), findsOneWidget);
+    // expect(find.text('Interest Rate'), findsNothing);
+
+    // await tester.enterText(find.byType(TextFormField), '225401');
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+  });
+}
